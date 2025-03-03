57fc89c05b4e6fcfe88b0c32c78c34fa0237ab8f
Mohammad Kahfi <mhmmdkahfi2000@gmail.com>
2025-03-03 13:52:35 +0700

    absoulte floating button

--- components/floating-action-buttons.tsx
+++ components/floating-action-buttons.tsx
@@ -5,7 +5,7 @@ import AddIcon from '@mui/icons-material/Add';
 export default function FloatingActionButtons() {
   return (
     <Box sx={{ '& > :not(style)': { m: 1 } }}>
-      <Fab color="primary" aria-label="add">
+      <Fab color="primary" aria-label="add" sx={{ position: 'fixed', bottom: 16, right: 16 }}>
         <AddIcon />
       </Fab>
     </Box>
