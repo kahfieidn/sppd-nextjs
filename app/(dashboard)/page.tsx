a208daf104eaaa80ea2b0ec294d57865db25fb1c
Mohammad Kahfi <mhmmdkahfi2000@gmail.com>
2025-03-03 15:35:48 +0700

    toolpad integration

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import QuickFilteringGrid from "@/components/quick-filtering-grid";
+import FloatingActionButtons from "@/components/floating-action-buttons";
+const Page = () => {
+  return (
+    <div>
+      <QuickFilteringGrid></QuickFilteringGrid>
+      <FloatingActionButtons></FloatingActionButtons>
+    </div>
+  );
+};
+
+export default Page;
