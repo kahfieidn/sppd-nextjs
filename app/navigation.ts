9b39078476ffd52dc45faf1b74fc145278192ab6
Mohammad Kahfi <mhmmdkahfi2000@gmail.com>
2025-03-04 10:37:54 +0700

    fix nav toolpad

---
+++ app/navigation.ts
@@ -0,0 +1,9 @@
+import DashboardIcon from "@mui/icons-material/Dashboard";
+
+export const NAVIGATION = [
+    {
+      segment: "/",
+      title: "Dashboard",
+      icon: DashboardIcon,
+    },
+  ];
