ad6cdd517cb41aabb9de440d734365066d4d5b3c
Mohammad Kahfi <mhmmdkahfi2000@gmail.com>
2025-03-05 17:39:42 +0700

    fix

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,15 @@
+import FloatingActionButtons from "@/components/floating-action-buttons";
+import BasicTable from "@/components/basic-table";
+const Page = () => {
+  return (
+    <div>
+
+      <div className="mt-10">
+      <BasicTable></BasicTable>
+      </div>
+      <FloatingActionButtons></FloatingActionButtons>
+    </div>
+  );
+};
+
+export default Page;
