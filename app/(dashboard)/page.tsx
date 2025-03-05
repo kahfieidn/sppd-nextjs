78d376a16fef3b5fa6355b6ce8ab6b114c1050b1
Mohammad Kahfi <mhmmdkahfi2000@gmail.com>
2025-03-05 17:44:56 +0700

    basic table

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,11 @@
 import FloatingActionButtons from "@/components/floating-action-buttons";
-// import BasicTable from "@/components/basic-table";
+import BasicTable from "@/components/basic-table";
 const Page = () => {
   return (
     <div>
         <h1>Dashboard</h1>
       <div className="mt-10">
-      {/* <BasicTable></BasicTable> */}
+      <BasicTable></BasicTable>
       </div>
       <FloatingActionButtons></FloatingActionButtons>
     </div>
